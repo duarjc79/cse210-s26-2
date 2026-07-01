53fea1e6c8f4f4a21a7aa32242399e404901b350
duarjc79 <dua24004@byui.edu>
2026-07-01 15:08:51 -0600

    prep 1 and 2 working

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,9 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
+Console.WriteLine("--- Basic Adder ---");
+Console.WriteLine("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1 + num2;
+Console.WriteLine($"The result of {num1} + {num2} is: {sum}");
