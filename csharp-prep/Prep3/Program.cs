eeb488e7a67078c0d934456a92a2634924672288
duarjc79 <dua24004@byui.edu>
2026-07-01 15:03:21 -0600

    prep 3 working

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main (string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+       Random randomGenerator = new Random();
+       int magicNum = randomGenerator.Next(1, 100);
+
+       int personGuess = 0;
+       int guessCount = 0;
+
+        do
+        {
+           Console.Write("What is your guess? ");
+           personGuess = Convert.ToInt32(Console.ReadLine());
+       
+       if (personGuess < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (personGuess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else Console.WriteLine("You guessed it!");
+
+            guessCount++;
+
+        } while (personGuess != magicNum);
+
+        Console.Write($"It took you {guessCount} guesses to guess the correct number!");
+           
+        }
+
 }
